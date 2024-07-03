2f43e2aeb14a142417f2c1137e6a622e713fc8d2
NNAANNAA1 <157097850+NNAANNAA1@users.noreply.github.com>
2024-07-03 10:10:25 +0700

    Create lk_info.php

---
+++ lk_info.php
@@ -0,0 +1,33 @@
+<body>
+    <div class="container">
+        <div class="profile-info">
+            <img src="image.png" alt="Аватар" class="avatar">
+            <div class="user-details">
+                <h2>Анастасия Орехова</h2>
+                <p>Телефон: +7 999 123 85 55</p>
+                <p>Почта: nastya@mail.com</p>
+            </div>
+        </div>
+
+        <div class="content">
+            <h3>Мой влог</h3>
+            <p>Я — человек, который ценит баланс в жизни. Работу воспринимаю как возможность развиваться и расти, но не забываю о важности личного времени и увлечений. </p>
+            <p>Фотография - это моя страсть, способ запечатлеть моменты и делиться ими с близкими. Люблю ловить свет, экспериментировать с композицией и передавать эмоции через объектив. </p>
+            <div class="container1">
+        <div class="photo">
+            <img src="photo1.jpg" alt="Фото 1">
+        </div>
+        <div class="photo">
+            <img src="photo2.jpg" alt="Фото 2">
+        </div>
+        <div class="photo">
+            <img src="photo3.jpg" alt="Фото 3">
+        </div>
+        <div class="photo">
+            <img src="photo4.jpg" alt="Фото 4">
+        </div>
+      
+    </div>
+        </div>
+    </div>
+</body>
